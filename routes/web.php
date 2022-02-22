46419a1f0f0104edc79419302a48e083fadbaa89
mfajririki <fajririki07@gmail.com>
2022-02-22 11:25:24 +0700

    update import excel

--- routes/web.php
+++ routes/web.php
@@ -47,15 +47,30 @@ Route::resource('/announcement', AnnouncementController::class);
 Route::get('/announcement/{id}/delete_doc', [AnnouncementController::class, 'delete_doc'])->name('announcement.delete_doc');
 Route::resource('/panduan', PanduanController::class);
 Route::get('/panduan/{id}/hapus_doc', [PanduanController::class, 'hapus_doc'])->name('panduan.hapus_doc');
+
 Route::resource('/staf_pengajar', StafController::class);
+Route::post('/staf/import_excel', [StafController::class, 'import_excel']);
+Route::get('/staf/delete_all', [StafController::class, 'delete_all']);
+
 Route::resource('/bidangminat', BidangMinatController::class);
+Route::post('/minat/import_excel', [BidangMinatController::class, 'import_excel']);
+Route::get('/minat/delete_all', [BidangMinatController::class, 'delete_all']);
+
 Route::resource('/visi_misi', VisiMisiController::class);
+
 Route::resource('/profile_lulusan', ProfileLulusanController::class);
+Route::post('/profilelulusan/import_excel', [ProfileLulusanController::class, 'import_excel']);
+Route::get('/profilelulusan/delete_all', [ProfileLulusanController::class, 'delete_all']);
+
 Route::resource('/pembimbing_akademik', PembimbingAkademikController::class);
 Route::resource('/tujuans', TujuansController::class);
 Route::resource('/tentang', TentangController::class);
+
 Route::resource('/kurikulums', KurikulumsController::class);
 Route::get('/kurikulums/{id}/hapus_doc', [KurikulumsController::class, 'hapus_doc'])->name('kurikulums.hapus_doc');
+Route::post('/kurikulum/import_excel', [KurikulumsController::class, 'import_excel']);
+Route::get('/kurikulum/delete_all', [KurikulumsController::class, 'delete_all']);
+
 Route::resource('/profile_kaprodi', ProfileKaprodiController::class);
 Route::resource('/slider', SliderController::class);
 Route::resource('/user', UsersController::class);
